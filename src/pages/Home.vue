ec05f8ad38697c3aa3e9f818d81f77b9e4e4efc0
Karyna Kananovich <karyna.kananovich@gmail.com>
2021-09-06 23:37:22 +0300

    feat: add Search component

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,8 +2,42 @@
   <div class="wrapper-content wrapper-content--fixed">
     <section>
       <div class="container">
-        <h1>Home page</h1>
+        <div class="content">
+          <search
+            :value="search"
+            placeholder="Type username..."
+            @search="search = $event"
+          />
+          <button @click="getRepos" class="btn btnPrimary">Search</button>
+        </div>
       </div>
     </section>
   </div>
 </template>
+
+<script>
+import search from "@/components/Search.vue";
+
+export default {
+  components: { search },
+  data() {
+    return {
+      search: "",
+    };
+  },
+  methods: {
+    getRepos() {
+      console.log(`get user ${this.search} repos`);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.content {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 24px;
+}
+</style>
